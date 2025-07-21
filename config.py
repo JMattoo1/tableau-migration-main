1227a09ed0917c37fd3aa0ce1340332a32b309d8
JMattoo1 <jayati.mattoo@rackspace.com>
2025-07-21 17:12:00 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# Enable customized conversion
+custom = True
+# translate from language
+fromLanguage = "tableau"
+# translate to language
+toLanguage = "dax"
+# input folder name
+inFolderName = "Dashboard"
+# input folder path
+inFolderPath = "C:/Users/zhi4949/projects/Tableau_PBI_Migration/"
+# output folder path
+outFilePath = "C:/Users/zhi4949/projects/Tableau_PBI_Migration/metadata_parse_file/Outputs/Report-Tableau-ALL-"
